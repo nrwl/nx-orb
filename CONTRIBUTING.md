dc3ca824f5f33296299d4576034b928f4372cdc1
Miroslav Jonaš <meeroslav@users.noreply.github.com>
2021-07-23 17:47:40 +0200

    [semver:major] feat: add set-shas command (#1)
    * feat: set-sha-command
    
    * [semver: major]: feat: add set-shas command
    
    * [semver: skip]: docs: update readme
    
    * feat(set-shas): move all logic to node script
    
    * feat(set-shas): add sha information to output
    
    * feat(set-shas): update examples
    
    * docs(set-shas): add usage to README.md
    
    * fix(set-shas): remove bats reference
    
    * fix(set-shas): remove shellcheck
    
    * fix: command structure
    
    * feat(circleci): extract project slug from build url
    
    * chore(circleci): update path
    
    * chore(circleci): include script
    
    * chore(circleci): inject script

---
+++ CONTRIBUTING.md
@@ -0,0 +1,61 @@
+# Contributing to nx-orb
+
+We welcome [issues](https://github.com/nrwl/nx-orb/issues) to and [pull requests](https://github.com/nrwl/nx-orb/pulls) against this repository! Read this document to see how to do it.
+
+## How to Contribute
+* Create and push a branch with your new features.
+* When ready to publish a new production version, create a Pull Request from _feature branch_ to `master`.
+* The title of the pull request should follow [commit message guideline](#commit-message-guideline)
+* Squash and merge. Ensure the semver tag is preserved and entered as a part of the commit message.
+* On merge, after manual approval, the orb will automatically be published to the Orb Registry.
+
+#### Commit Message Guidelines
+
+The commit message should follow the following format:
+
+```
+[semver:<segment>] type: subject
+BLANK LINE
+body
+```
+
+##### Semver tag
+
+The title of the pull request must contain a special semver tag: `[semver:<segment>]` where `<segment>` is replaced by one of the following values.
+
+| Increment | Description|
+| ----------| -----------|
+| major     | Issue a 1.0.0 incremented release|
+| minor     | Issue a x.1.0 incremented release|
+| patch     | Issue a x.x.1 incremented release|
+| skip      | Do not issue a release|
+
+Example: `[semver:major]`
+
+##### Type
+
+The type must be one of the following:
+
+- feat - New or improved behavior being introduced (e.g. adding new command)
+- fix - Fixes the current unexpected behavior to match expected behavior (e.g. fixing the wrong commit search logic)
+- cleanup - Code Style changes that have little to no effect on the user (e.g. Refactoring some function)
+- docs - Changes to the documentation (e.g. Adding more details into the examples)
+- chore - Changes that have absolutely no effect on users (e.g. Renaming files)
+
+Optionally, type can have a scope (e.g. `feat(set-shas)`)
+
+##### Subject and Body
+
+The subject must contain a description of the change, and the body of the message contains any additional details to provide more context about the change.
+
+Including the issue number that the PR relates to also helps with tracking.
+
+##### Example
+
+```
+[semver:minor] feat(set-shas): add optinal workflow-id
+
+Add workflow-id to filter successful workflows
+
+Closes #157
+```
---
+++ src/scripts/find-successful-workflow.js
@@ -0,0 +1,114 @@
+#!/usr/bin/env node
+const { execSync } = require('child_process');
+const https = require('https');
+
+const buildUrl = process.argv[2];
+const branchName = process.argv[3];
+const mainBranchName = process.argv[4];
+const errorOnNoSuccessfulWorkflow = process.argv[5];
+
+let BASE_SHA;
+(async () => {
+  if (branchName !== mainBranchName) {
+    BASE_SHA = execSync(`git merge-base origin/${mainBranchName} HEAD`, { encoding: 'utf-8' });
+  } else {
+    try {
+      BASE_SHA = await findSuccessfulCommit(buildUrl, mainBranchName);
+    } catch (e) {
+      process.stderr.write(e.message);
+      process.exit(1);
+    }
+
+    if (!BASE_SHA) {
+      if (errorOnNoSuccessfulWorkflow === 'true') {
+        process.stdout.write(`
+    Unable to find a successful workflow run on 'origin/${mainBranchName}'
+    NOTE: You have set 'error-on-no-successful-workflow' on the step so this is a hard error.
+
+    Is it possible that you have no runs currently on 'origin/${mainBranchName}'?
+    - If yes, then you should run the workflow without this flag first.
+    - If no, then you might have changed your git history and those commits no longer exist.`);
+        process.exit(1);
+      } else {
+        process.stdout.write(`
+WARNING: Unable to find a successful workflow run on 'origin/${mainBranchName}'.
+We are therefore defaulting to use HEAD~1 on 'origin/${mainBranchName}'.
+
+NOTE: You can instead make this a hard error by settting 'error-on-no-successful-workflow' on the step in your workflow.\n\n`);
+        BASE_SHA = execSync(`git rev-parse HEAD~1`, { encoding: 'utf-8' });
+      }
+    } else {
+      process.stdout.write(`
+Found the last successful workflow run on 'origin/${mainBranchName}'.\n\n`);
+    }
+  }
+
+  process.stdout.write(`Commit: ${BASE_SHA}\n`);
+})();
+
+async function findSuccessfulCommit(buildUrl, branch) {
+  const project = buildUrl.match(/https:\/\/circleci.com\/(.*)\/\d./)[1];
+  const url = `https://circleci.com/api/v2/project/${project}/pipeline?branch=${branch}`;
+
+  let nextPage;
+  let foundSHA;
+
+  do {
+    const fullUrl = nextPage ? `${url}&page-token=${nextPage}` : url;
+    const { next_page_token, sha } = await getJson(fullUrl)
+      .then(async ({ next_page_token, items }) => {
+        const pipeline = await findSuccessfulPipeline(items);
+        return {
+          next_page_token,
+          sha: pipeline ? pipeline.vcs.revision : void 0
+        };
+      });
+
+    foundSHA = sha;
+    nextPage = next_page_token;
+  } while (!foundSHA && nextPage);
+
+  return foundSHA;
+}
+
+async function findSuccessfulPipeline(pipelines) {
+  for (const pipeline of pipelines) {
+    if (!pipeline.errors.length
+      && commitExists(pipeline.vcs.revision)
+      && await isWorkflowSuccessful(pipeline.id)) {
+      return pipeline;
+    }
+  }
+  return undefined;
+}
+
+function commitExists(commitSha) {
+  try {
+    execSync(`git cat-file -e ${commitSha} 2> /dev/null`);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+async function isWorkflowSuccessful(pipelineId) {
+  return getJson(`https://circleci.com/api/v2/pipeline/${pipelineId}/workflow`)
+    .then(({ items }) => items.every(item => item.status === 'success'));
+}
+
+async function getJson(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, res => {
+      let data = [];
+
+      res.on('data', chunk => {
+        data.push(chunk);
+      });
+
+      res.on('end', () => {
+        const response = Buffer.concat(data).toString();
+        resolve(JSON.parse(response));
+      });
+    }).on('error', error => reject(error));
+  });
+}
